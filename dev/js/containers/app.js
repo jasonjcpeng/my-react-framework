215e8d26cf30d9af91143ba89c2e8ecfeaa497d0
JasonJCpeng <jasonjcpeng@hotmail.com>
2017-06-09 10:29:39 +0800

    'update'

--- dev/js/containers/app.js
+++ dev/js/containers/app.js
@@ -2,11 +2,141 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as ActionCreators from '../action/app';
+import PropTypes from 'prop-types';
 import {List,Map} from 'immutable';
 
+class Student{
+    static varStatic = 21;
+    _name;
+    constructor(name){
+        this.name = name;
+    }
+
+    set name(name){
+        this._name = name;
+    }
+
+    sayName(){
+        console.log(this._name);
+    }
+    static sayStatic(){
+        return  this.varStatic;
+    }
+}
+
+class Teammate extends Student{
+    constructor(num){
+        super();
+        this.num = num;
+    }
+    sayHello(){
+        
+    }
+}
+
+class Node{
+    data;next;
+    constructor(data){
+        if(data){
+            this.data = data;
+            this.next = null;
+        }else{
+            this.next=null;
+        }
+    }
+}
+
+class NodeList{
+    head;
+    constructor(){
+        this.head = new Node(null);
+    }
+
+    add(data){
+        let current = this.head;
+        let newNode = new Node(data);
+        while(current.next!==null){
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    query(data){
+        let current = this.head;
+        while(current.next!==null){
+            if(current.data===data){
+                return current;
+            }
+            current = current.next;
+        }
+    }
+
+    makeLoop(target){
+        let loopTarget = this.query(target);
+        let current = this.head;
+        while(current.next!==null){
+            current = current.next;
+        }
+        current.next = loopTarget;
+    }
+
+    reserve(){
+        let current = this.head.next;
+        let p;
+        if(current===null){
+            return this.head;
+        }
+        while (current.next!==null){
+            p = current.next;
+            current.next = p.next;
+            p.next = this.head.next;
+            this.head.next = p;
+        }
+        return this.head;
+    }
+
+    hasLoop(){
+        let first = this.head;
+        let second = this.head;
+        while(first.next!==null&&second.next.next!==null){
+            first = first.next;
+            second = second.next.next;
+            if(first.next === second.next.next){
+                return first;
+            }
+        }
+        return false;
+    }
+
+    loopLength(){
+        let point = this.hasLoop();
+        let length = 2;
+        let current = this.hasLoop();
+        while(current.next!==point){
+            current = current.next;
+            length++;
+        }
+        return length;
+    }
+
+}
+
 class App extends React.Component {
+    
     render() {
         let {actionClick,click} =this.props;
+        let list = new NodeList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.makeLoop(3);
+        console.log(list);
+        console.log(list.hasLoop());
+        console.log(list.loopLength());
         return <div className="app">
             <h1>{click.get(0)}</h1>
             <button onClick={
@@ -17,10 +147,14 @@ class App extends React.Component {
         </div>;
     }
 }
+App.propTypes = {
+    click:PropTypes.object.isRequired
+}
+
 
 function state(state) {
     return ({
-        click:state.common.get('onClick')
+        click:state.common.get('onClick'),
     });
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var isDist = false;
 var Tittle = 'framework';
-module.exports = {
-    //不打包的基本库，可以做CDN加速
-   /* externals: {
-        'react': 'window.React',
-        'react-dom': 'window.ReactDOM',
-        'redux':'window.Redux',
-        'react-redux':'window.ReactRedux'
-    },*/
+module.exports = isDist ? {
+    //不打包的库并引向全局变量
+    /*externals: {
+     'react': 'window.React',
+     'react-dom': 'window.ReactDOM',
+     'redux': 'window.Redux',
+     'react-redux': 'window.ReactRedux'
+     },*/
     devServer: {
         lazy: false,
         watchOptions: {
@@ -20,9 +21,9 @@ module.exports = {
         contentBase: './dev',
         port: 3003
     },
-    devtool: 'cheap-module-eval-source-map',
-    entry: { index:'./dev/js/index.js'
-       /* ,vendor: ['react', 'react-dom', 'react-router','redux','react-redux','classnames']*/
+    entry: {
+        index: './dev/js/index.js'
+        /* ,vendor: ['react', 'react-dom', 'react-router','redux','react-redux','classnames']*/
     },
     module: {
         loaders: [
@@ -40,8 +41,8 @@ module.exports = {
                 loader: 'style-loader!css-loader!postcss-loader'
             },
             {
-                test:/\.(ttf|jpg|gif|png)$/,
-                loader:'file-loader?name=src/[name].[ext]'
+                test: /\.(ttf|jpg|gif|png)$/,
+                loader: 'file-loader?name=src/[name].[ext]'
             },
             {
                 test: /\.json$/,
@@ -50,7 +51,7 @@ module.exports = {
         ]
     },
     output: {
-        path: 'dist/',
+        path: path.resolve(__dirname, "dist"),
         filename: 'js/[chunkHash:32].js',
     },
     plugins: [
@@ -59,24 +60,87 @@ module.exports = {
             template: 'dev/index.html',
             filename: 'index.html',
             inject: 'body',
-            chunks:['index']
+            chunks: ['index']
         }),
-        /*new webpack.DefinePlugin({
+        new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: JSON.stringify('production')
             }
         }),
-        /!* 混淆压缩*!/
         new webpack.optimize.UglifyJsPlugin({
-         compress: {
-         warnings: false
-         }
-         }),*/
+            compress: {
+                warnings: false
+            }
+        }),
         /* 生成公共文件
          new webpack.optimize.CommonsChunkPlugin({
          name:'index', // 上面入口定义的节点组
          filename:'./js/bundle.login.js' //最后生成的文件名
          }),*/
+        new webpack.ProvidePlugin({
+            React: 'react'
+        })//解决全打包状态下丢失全局变量window.React问题
+    ]
+} : {
+    //不打包的基本库，可以做CDN加速
+    /* externals: {
+     'react': 'window.React',
+     'react-dom': 'window.ReactDOM',
+     'redux':'window.Redux',
+     'react-redux':'window.ReactRedux'
+     },*/
+    devServer: {
+        lazy: false,
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000
+        },
+        inline: true,
+        contentBase: './dev',
+        port: 3003
+    },
+    devtool: 'cheap-module-eval-source-map',
+    entry: {
+        index: './dev/js/index.js'
+        /* ,vendor: ['react', 'react-dom', 'react-router','redux','react-redux','classnames']*/
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['babel-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!less-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader!postcss-loader'
+            },
+            {
+                test: /\.(ttf|jpg|gif|png)$/,
+                loader: 'file-loader?name=src/[name].[ext]'
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'js/[chunkHash:32].js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: Tittle,
+            template: 'dev/index.html',
+            filename: 'index.html',
+            inject: 'body',
+            chunks: ['index']
+        }),
         new webpack.ProvidePlugin({
             React: 'react'
         })//解决不用CDN的全打包状态下丢失全局变量window.React问题
